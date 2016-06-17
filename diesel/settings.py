a488f8c9e04375188b01f9f7beb9e99b296579c9
Hao Chen <hach@microsoft.com>
2016-06-17 15:21:40 -0700

    update db settings to mysql

--- diesel/settings.py
+++ diesel/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'diesel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'default_db',
+        'USER': 'default_user',
+        'PASSWORD': 'default_password',
+        'HOST': 'localhost',
+        'PORT': '3306'
+
     }
 }
 
