69518953a56fe701fbb20755071363361583765b
Hao Chen <hach@microsoft.com>
2016-06-20 18:31:12 -0700

    create post schema

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+# Post
+# Tag
+# Tag_Post
+# Comment
+
+class PostManager(models.Manager):
+    def create_post(self, title):
+        post = self.create()
+        return post
+
+class Post(models.Model):
+    PRIVACY_OPTS = (
+        ('all', 'public'),
+        ('myself', 'youself'),
+    )
+
+    STATUSES = (
+        ('d', 'draft'),
+        ('p', 'published'),
+    )
+
+    # Defin Post schema
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    created_date = models.DateTimeField(default=datetime.now, blank=True)
+    last_modify_date = models.DateField()
+    privacy = models.CharField(max_length=10, choices=PRIVACY_OPTS, default=PRIVACY_OPTS[0][0], blank=True)
+    deleted = models.BooleanField(default=False, blank=True)
+    deleted_date = models.DateField(null=True, blank=True)
+    status = models.CharField(max_length=1, choices=STATUSES, default=STATUSES[0][0], blank=True)
+
+    objects = PostManager()
+
+
+
