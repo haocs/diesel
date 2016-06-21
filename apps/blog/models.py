866df90036a8890ad44bb12b367b46630b225df0
Hao Chen <hach@microsoft.com>
2016-06-20 20:17:05 -0700

    update models for blog

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -6,15 +7,20 @@ from django.db import models
 # Tag_Post
 # Comment
 
+
 class PostManager(models.Manager):
-    def create_post(self, title):
-        post = self.create()
+    def create_post(self, user_id, title, content, privacy, status, tags):
+        post = self.create(user=user_id, title=title, content=content, privacy=privacy, status=status)
+        if tags:
+            # add new tags and update TagPost
+            pass
         return post
+
 
 class Post(models.Model):
     PRIVACY_OPTS = (
         ('all', 'public'),
-        ('myself', 'youself'),
+        ('myself', 'yourself'),
     )
 
     STATUSES = (
@@ -22,10 +28,10 @@ class Post(models.Model):
         ('p', 'published'),
     )
 
-    # Defin Post schema
+    # Define Post schema
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=256)
-    content = models.TextField()
+    content = models.TextField(default='', blank=True)
     created_date = models.DateTimeField(default=datetime.now, blank=True)
     last_modify_date = models.DateField()
     privacy = models.CharField(max_length=10, choices=PRIVACY_OPTS, default=PRIVACY_OPTS[0][0], blank=True)
@@ -36,4 +42,12 @@ class Post(models.Model):
     objects = PostManager()
 
 
+class Tag(models.Model):
+    tag = models.CharField(max_length=20)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class TagPost(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
