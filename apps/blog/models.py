fd7bca2b3b55863d1115db16a49b01c110206193
Hao Chen <c@haoc.io>
2016-08-23 17:26:37 -0700

    update blog models

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -2,10 +2,12 @@ from datetime import datetime
 from django.contrib.auth.models import User
 from django.db import models
 
-# Post
-# Tag
-# Tag_Post
-# Comment
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
 
 
 class Post(models.Model):
@@ -20,8 +22,8 @@ class Post(models.Model):
     )
 
     LANGUAGES = (
-        ('cn', 'chinese'),
         ('en', 'english'),
+        ('cn', 'chinese'),
     )
 
     # Define Post schema
@@ -29,20 +31,13 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=256)
     content = models.TextField(default='', blank=True)
+    tags = models.ManyToManyField(Tag)
     created_timestamp = models.DateTimeField(default=datetime.now, blank=True)
     mod_timestamp = models.DateField(default=datetime.now, blank=True)
-    privacy = models.CharField(max_length=10, choices=PRIVACY_OPTS, default=PRIVACY_OPTS[0][0], blank=True)
     deleted = models.BooleanField(default=False, blank=True)
     del_timestamp = models.DateField(null=True, blank=True)
+    privacy = models.CharField(max_length=10, choices=PRIVACY_OPTS, default=PRIVACY_OPTS[0][0], blank=True)
     status = models.CharField(max_length=1, choices=STATUSES, default=STATUSES[0][0], blank=True)
-
-    def __str__(self):              # __unicode__ on Python 2
-        return self.name
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=20)
-    posts = models.ManyToManyField(Post)
 
     def __str__(self):              # __unicode__ on Python 2
         return self.name
