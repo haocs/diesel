46adf5b4838e91e95752a9cd8474079f8415b307
Hao Chen <c@haoc.io>
2016-08-23 17:26:21 -0700

    add tests

--- apps/blog/tests/test_models.py
+++ apps/blog/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.auth.models import User
 
-from apps.blog.models import Post
+from apps.blog.models import Post, Tag
 
 
 class PostTestCase(TestCase):
@@ -18,9 +18,18 @@ class PostTestCase(TestCase):
         self.assertEqual(post1.content, post_get.content)
         self.assertEqual(post1.status, post_get.status)
         self.assertEqual(post1.author.username, post_get.author.username)
+        self.assertEqual('en', post_get.language)
+        self.assertEqual('p', post_get.status)
+        self.assertEqual('all', post_get.privacy)
 
     def test_create_post_with_tags_should_pass(self):
-        pass
-
-    def test_create_tag_should_pass(self):
-        pass
+        post1 = Post(author=self.test_user, title='test post 1', content='test content 1', status='p')
+        post1.save()
+        tag1 = Tag(name='testtag1')
+        tag1.save()
+        tag2 = Tag(name='testtag2')
+        tag2.save()
+        post1.tags.add(tag1, tag2)
+        post_get = Post.objects.latest('id')
+        self.assertEqual(post1.title, post_get.title)
+        self.assertEqual(len(post_get.tags.all()), 2)
