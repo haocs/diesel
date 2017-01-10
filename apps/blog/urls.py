b5fcdbeca59d8dca27d8dc481733e9c245a3935f
Hao Chen <git@haoc.io>
2017-01-10 00:43:17 -0800

    try to restify blog api

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.list_posts),
-    url(r'^', views.create_post)
+    url(r'^(?P<post_id>[0-9]+)$', views.post_handle),
+    # uri could be used for get/put/delete
+    url(r'^(?P<uri>[a-z|A-Z|0-9|_]+)$', views.post_handle),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,16 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Post, Tag
 
+import json
+
+
+@csrf_exempt
 def post_handle(request, **kwargs):
     if request.method == 'GET':
-        get_post(request, kwargs)
+        return get_post(request, **kwargs)
+    elif request.method == 'POST':
+         return create_post(request, **kwargs)
+    elif request.method == 'PUT':
+        update_post(request, **kwargs)
+    elif request.method == 'DELETE':
+        pass
 
-def get_post(request, uri):
-    pass
+def get_post(request, **kwargs):
+    return HttpResponse('ok')
 
 def create_post(request, **kwargs):
-    pass
+    if not request.body:
+        return HttpResponse('error')
+    print(kwargs.get('uri'))
+    print(kwargs.get('post_id'))
+    post_json = json.loads(request.body.decode('utf-8'))
+    print(post_json)
+    print(type(post_json))
+    title = post_json.get('title')
+    content = post_json['title']
+    tags = post_json.get('tags')
+    return HttpResponse('ok')
 
 def update_post(request, **kwargs):
     pass
